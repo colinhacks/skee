a9746861d4c3da0f8dc60fa46007f9b3d78d7799
Colin McDonnell <colinmcd@alum.mit.edu>
2020-07-21 16:50:55 -0700

    Patch

--- src/external.ts
+++ src/external.ts
@@ -1,3 +1,3 @@
 import { Skee } from './internal';
 export const create = Skee.create;
-export { Skee, COLUMN, FUNCTION, EDGE, Column, Edge, Schema, RelationalSchema, OOPSchema } from './internal';
+export { Skee, COLUMN, FUNCTION, EDGE, Column, Edge, Schema, RelationalSchema, OOPSchema, DB } from './internal';
